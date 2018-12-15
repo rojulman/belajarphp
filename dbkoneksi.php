2254ce413e118f270f4af076a5fe5431732e1b9c
Sirojul Munir <rojulman@gmail.com>
2018-12-15 10:04:51 +0700

    dbkoneksi

--- dbkoneksi.php
+++ dbkoneksi.php
@@ -1,13 +1,15 @@
 <?php 
 try {
+   // 1) siapkan variable DSN	
    $host = 'localhost';
    $dbname = 'dbkegiatan';
    $dbuser = 'siswa';
-   $dbpass = '123456';
+   $dbpass = '123456'; 
 
-   $dbh = new PDO("pgsql:host=$host;dbname=$dbname",
+   // 2) buat objek koneksi
+    $dbh = new PDO("pgsql:host=$host;dbname=$dbname",
    	$dbuser,$dbpass);
-   $dbh->setAttribute(PDO::ATTR_ERRMODE ,
+    $dbh->setAttribute(PDO::ATTR_ERRMODE ,
      PDO::ERRMODE_EXCEPTION
    	);
 }catch(PDOException $e){
